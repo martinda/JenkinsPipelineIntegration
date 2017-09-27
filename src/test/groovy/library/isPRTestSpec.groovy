b0ef2caae58f6df492b9e10493980e4a7e2f90cc
Martin d'Anjou <martin.danjou14@gmail.com>
2017-09-26 21:00:58 -0400

    Test http request

--- src/test/groovy/library/isPRTestSpec.groovy
+++ src/test/groovy/library/isPRTestSpec.groovy
@@ -8,41 +8,28 @@ import hudson.model.Result;
 
 class isPRTestSpec extends PipelineTestBase {
 
-    def "isPR returns true when env.BRANCH_NAME starts with PR-"() {
+    def "http request"() {
 
         given:
             WorkflowJob p = j.createProject(WorkflowJob.class, "p");
 
         when:
             p.setDefinition(new CpsFlowDefinition("""
-                node(){ 
-                    env.BRANCH_NAME = 'PR-123'; 
-                    if(!isPR()) {
-                        sh 'exit 1'
-                    } 
-                }"""
+                node() {
+                    def response = httpRequest(
+                        url: 'http://google.com/',
+                        responseHandle: 'STRING',
+                        validResponseCodes: '101:399'
+                    )
+                    println(response.content)
+                    println(response.status)
+                    response.close()
+                }
+                """
             ));
 
         then:
-            j.assertBuildStatusSuccess(p.scheduleBuild2(0));
+            j.assertBuildStatus(Result.SUCCESS,p.scheduleBuild2(0));
     }
 
-    def "isPR returns false when env.BRANCH_NAME starts with a normal branch name"() {
-
-        given:
-            WorkflowJob p = j.createProject(WorkflowJob.class, "p");
-
-        when:
-            p.setDefinition(new CpsFlowDefinition("""
-                node(){ 
-                    env.BRANCH_NAME = 'master'; 
-                    if(!isPR()) {
-                        sh 'exit 1'
-                    } 
-                }"""
-            ));
-
-        then:
-            j.assertBuildStatus(Result.FAILURE,p.scheduleBuild2(0));
-    }
 }
